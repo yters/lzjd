c6ea8bca05ad922bb233ce594051fcc2962e358c
yters <eric.holloway@gmail.com>
2020-04-12 21:33:37 -0400

    Removed unneeded code from pairs

--- src/cluster.py
+++ src/cluster.py
@@ -6,8 +6,8 @@ for line in open(sys.argv[1]).read().splitlines():
     a, b, d = line.split('|')
     d = int(d)
     pairs += [(a,b,d)]
+
 clusters = []
-hubs = []
 for p in pairs:
     a, b, d = p
     c_a = None
@@ -20,5 +20,6 @@ for p in pairs:
     if not c_a and not c_b: clusters += [set([a, b])]
     elif not c_a: c_b.add(a)
     elif not c_b: c_a.add(b)
+
 for i, c in enumerate(clusters):
     print('cluster ' + str(i) + ': ' + ', '.join(c))
