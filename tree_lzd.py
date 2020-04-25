69c3b13de8b5b21511f09f9e5b64fae19d5bd052
yters <eric.holloway@gmail.com>
2020-04-25 01:09:49 -0400

    find candidates function

--- tree_lzd.py
+++ tree_lzd.py
@@ -1,4 +1,25 @@
 nodes = []
+def backtrack(parent, curr, count):
+    if parent == -1:
+        return curr
+    if count == 0:
+        return curr
+    return backtrack(nodes[parent][0], parent, count-1)
+        
+def find_candidates(start, symbol):
+    candidates = []
+    open_list = nodes[start][2].values()
+    closed_list = set()
+    while len(open_list) > 0:
+        curr = open_list.pop()
+        closed_list.add(curr)
+        for potential in nodes[curr][2].values():
+            if not potential in closed_list:
+                open_list.append(potential)
+        if symbol in nodes[curr][2]:
+            candidates.append(nodes[curr][2][symbol])
+    return candidates
+
 def tree_lzd_list(sequence):
     global nodes
     tree = (-1, len(nodes), {}, -1)
@@ -9,17 +30,20 @@ def tree_lzd_list(sequence):
             curr = nodes[curr][2][symbol]
         else:
             new_node = None
+            print(find_candidates(nodes[curr][0], symbol))
             if not nodes[curr][0] == -1:
                 for ancestor in nodes[nodes[curr][0]][2].values():
                     if symbol in nodes[ancestor][2]:
                         new_node = nodes[nodes[ancestor][2][symbol]]
+            #new_node = None
             if not new_node:
-                new_node = (curr, len(nodes), {}, i)
+                new_node = (curr, len(nodes), {}, i, symbol)
                 nodes += [new_node]
             nodes[curr][2][symbol] = new_node[1]
             curr = 0
 
 if __name__ == "__main__":
-    seq = 'aagttgccgggg'
+    seq = 'aagttg'
     tree = tree_lzd_list(seq)
     print(nodes)
+    print(len(nodes))
